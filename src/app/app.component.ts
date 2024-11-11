e7c92b8be49131f6c17e7a3742f7f70fd7e04834
CPenalba <carolinapenalbacorpa@gmail.com>
2024-11-11 09:48:36 +0100

    Personas standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonasstandaloneComponent } from './components/personasstandalone/personasstandalone.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonasstandaloneComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angularstandalone';
